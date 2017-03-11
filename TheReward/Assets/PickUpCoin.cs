ea608adb8c3d49cbd9e5b329cccfed59bb544629
Yaroslav Kravtsov <yaroslav.kravtsov@gmail.com>
2017-03-11 15:01:51 +0300

    Coin for pickup

---
+++ TheReward/Assets/PickUpCoin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpCoin : MonoBehaviour {
+
+	void OnCollisionEnter ( )
+	{
+		GameObject.Destroy ( gameObject ) ;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(Vector3.up * 100 * Time.deltaTime);
+	}
+}
