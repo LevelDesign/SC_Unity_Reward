489c4938d0201285bca7fb4e3c40c73314769d26
Yaroslav Kravtsov <yaroslav.kravtsov@gmail.com>
2017-03-11 16:18:44 +0300

    Shooting enemies

---
+++ TheReward/Assets/Prefabs/bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter ( )
+	{
+		GameObject.Destroy ( gameObject ) ;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * Time.deltaTime * 5f;
+	}
+}
